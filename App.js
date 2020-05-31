1421ae80814043ff05aaede79349ee572383344d
riya-the-coder <riyaridap17@gmail.com>
2020-05-31 13:46:44 +0530

    Started Wily App

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TransactionScreen from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <AppContainer/>
     </View>
   );
 }
+}
+const TabNavigator=createBottomTabNavigator({
+  Transaction:{screen:TransactionScreen},
+  Search:{screen:SearchScreen},
+});
+const AppContainer=createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
